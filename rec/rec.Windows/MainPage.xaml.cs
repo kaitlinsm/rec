130ac2967789d9c595cb81cb19e496f668f6ec91
kaitlinsm <kaitlinsmth@gmail.com>
2015-02-09 17:41:11 +0000

    first commit

---
+++ rec/rec.Windows/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace rec
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public Recording CurrentRecording { get; set; }
+
+        private TimeSpan currentMax = new TimeSpan(0, 0, 10);
+
+        public MainPage()
+        {
+            this.CurrentRecording = new Recording((s, ts) =>
+            {
+                var time = String.Format(
+                    "{0:00}:{1:00}:{2:00}", 
+                    ts.Hours, ts.Minutes, ts.Seconds);
+                var ms = String.Format(".{0:0}", ts.Milliseconds / 100);
+
+                this.txtTime.Text = time;
+                this.txtMs.Text = ms;
+
+                if (ts >= currentMax)
+                {
+                    currentMax = ts.TotalSeconds > 60 && ts.TotalMinutes < 2 ? new TimeSpan(0, 2, 0) :
+                        ts.TotalMinutes > 2 && ts.TotalMinutes < 5 ? new TimeSpan(0, 5, 0) :
+                        currentMax.Add(new TimeSpan(0, 5, 0));
+                }
+
+                this.pbRecording.Maximum = 100;
+                this.pbRecording.Value = 100 * ts.TotalMilliseconds / currentMax.TotalMilliseconds;
+                
+
+            });
+            
+            this.InitializeComponent();
+
+            UpdateMaxTimeDisplay();
+        }
+
+        private void UpdateMaxTimeDisplay()
+        {
+            this.txtMax.Text =
+                currentMax.TotalMinutes > 0 ? currentMax.TotalMinutes.ToString() + " minutes" :
+                currentMax.TotalSeconds.ToString() + " seconds";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentRecording.IsRecording)
+            {
+                CurrentRecording.Stop();
+            }
+            else
+            {
+                CurrentRecording.Start();
+            }
+                
+        }
+    }
+}
---
+++ rec/rec.Windows/Recording.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace rec
+{
+    public class Recording
+    {
+        private DispatcherTimer dt;
+        private Stopwatch sw;
+        private Action<object, TimeSpan> tickEventHanlder;
+
+
+        public Recording(Action<object, TimeSpan> tickEventHandler)
+        {
+            if (tickEventHandler == null)
+                throw new ArgumentNullException("tickEventHandler", "Can't be null!");
+
+            this.dt = new DispatcherTimer();
+            this.sw = new Stopwatch();
+            this.tickEventHanlder = tickEventHandler;
+            this.dt.Tick += sw_Tick;
+
+            this.dt.Interval = new TimeSpan(0, 0, 0, 0, 100);
+        }
+
+        void sw_Tick(object sender, object e)
+        {
+            this.tickEventHanlder(sender, this.sw.Elapsed);
+        }
+
+        internal void Start()
+        {
+            dt.Start();
+            sw.Start();
+        }
+
+        internal bool IsRecording
+        {
+            get
+            {
+                return this.sw.IsRunning;
+            }
+        }
+
+        internal void Stop()
+        {
+            dt.Stop();
+            sw.Stop();
+        }
+
+        
+        
+    }
+}
